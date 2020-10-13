278972d10cccb1dc4ecd0a4ff46d99b939c9ef72
Kevin VALERIO <valerio.kevin83@gmail.com>
2020-10-13 16:30:07 +0200

    tkt c wiki title

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,4 @@
+import org.jsoup.Jsoup
 import java.io.File
 
 
@@ -8,6 +9,9 @@ fun main(args: Array<String>) {
     val geoffrey: MutableList<String> =
         readFileAsLinesUsingUseLines("src/main/assets/Geoffrey Hinton.txt") as MutableList<String>
 
+//    println(getWikiTitle("https://fr.wikipedia.org/wiki/Kawasaki_ZRX"))
+
+
     geoffrey.forEach { line -> line.split(" ").forEach { text += (buildSentenceFromWord(it, entities) + " ") } }
     println(text)
 }
@@ -15,7 +19,10 @@ fun main(args: Array<String>) {
 fun buildSentenceFromWord(word: String, entities: List<String>): String {
     var tmp: String = word
     entities.forEach {
-        if (it.contains(word) && word.length >= 4) tmp = "<entity name=\"https://en.wikipedia.org/wiki/" + it.replace(" ", "_") + "\">" + word + "</entity>"
+        if (it.contains(word) && word.length >= 4) tmp = "<entity name=\"https://en.wikipedia.org/wiki/" + it.replace(
+            " ",
+            "_"
+        ) + "\">" + word + "</entity>"
     }
 
     return tmp
@@ -26,11 +33,16 @@ fun readFileLineByLineUsingForEachLine(fileName: String) = File(fileName).forEac
 fun readFileAndExtractEntities(fileName: String): List<String> = File(fileName).useLines {
     var list = it.toList()
     list = list.map { it2 ->
-        it2.split("/").last().replace("_"," ")
+        it2.split("/").last().replace("_", " ")
     }
     list
 }
 
 fun readFileAsLinesUsingUseLines(fileName: String): List<String> = File(fileName).useLines {
     it.toList()
+}
+
+@Throws(Exception::class)
+fun getWikiTitle(url: String?): String {
+    return Jsoup.connect(url).get().select("#firstHeading").first().childNodes().get(0).toString()
 }
